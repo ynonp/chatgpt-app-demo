64a8620924b738c4c17898b6c2ce005beb6ecdbc
ynonp <ynonperek@gmail.com>
2025-10-26 09:53:44 +0200

    initial commit

---
+++ main.ts
@@ -0,0 +1,100 @@
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { z } from "zod";
+import dadJokes from '@mikemcbride/dad-jokes'
+import { StreamableHTTPServerTransport } from '@modelcontextprotocol/sdk/server/streamableHttp.js';
+import express from 'express';
+
+// Create server instance
+const server = new McpServer({
+  name: "dadjokes",
+  version: "1.0.0",
+  capabilities: {
+    resources: {},
+    tools: {},
+  },
+});
+
+server.registerResource(
+  'jokes_count',
+  'jokes://count',
+  {
+    title: 'Jokes Count Resource',
+  },
+  async (uri) => {
+    return ({
+      contents: [
+        {
+          uri: uri.href,
+          text: `I know ${dadJokes.all.length} jokes`,
+        }
+      ]
+    })
+  }
+)
+
+server.registerTool(
+  "tell-me-a-joke",
+  {
+    inputSchema: {
+      id: z.number().min(0).max(dadJokes.all.length).describe('joke id')
+    },
+    title: 'Joke Teller',
+    description: `Tells a joke according to its index. Valid ids 0-${dadJokes.all.length - 1}`,    
+  },
+  async ({id}) => {
+    return ({
+      content: [
+        {
+          type: 'text',
+          text: dadJokes.all[id],
+        }
+      ]
+    })
+  }
+)
+
+server.registerTool(
+  "tell-me-a-random-joke",  
+  {
+    title: 'Random Joke Teller',
+    description: 'Tells a random joke',    
+  },
+  async () => {
+    return ({
+      content: [
+        {
+          type: 'text',
+          text: `${dadJokes.random()}`,
+        }
+      ]
+    })
+  }
+);
+
+const app = express();
+app.use(express.json());
+
+app.post('/mcp', async (req: any, res: any) => {
+    // Create a new transport for each request to prevent request ID collisions
+    const transport = new StreamableHTTPServerTransport({
+        sessionIdGenerator: undefined,
+        enableJsonResponse: true
+    });
+
+    res.on('close', () => {
+        transport.close();
+    });
+
+    await server.connect(transport);
+    await transport.handleRequest(req, res, req.body);
+});
+
+const port = parseInt(process.env.PORT || '3000');
+app.listen(port, () => {
+    console.log(`Demo MCP Server running on http://localhost:${port}/mcp`);
+}).on('error', error => {
+    console.error('Server error:', error);
+    process.exit(1);
+});
+
